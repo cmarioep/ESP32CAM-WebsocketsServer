181186f593b837ed4a7e0c9f6792c5204881e85f
cmarioep <cmescandonpardo@gmail.com>
2022-08-29 10:01:56 -0500

    Feature: set http and webSocket server config

---
+++ Server/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const WebSocket = require('ws');
+
+const httpServer = express();
+const HTTP_PORT = process.env.PORT || 3000;
+const WS_PORT = 8888;
+
+
+httpServer.listen(HTTP_PORT, () => {
+    console.log(`HTTP server listening at ${HTTP_PORT}`);
+}
+);
+
+
+const wsServer = new WebSocket.Server({ port: WS_PORT }, () => {
+    console.log(`WebSocket Server is listening at ${WS_PORT}`)
+}
+);
+
+
+let connectedClients = [];
+
+wsServer.on('connection', (ws, req) => {
+    console.log('Connected');
+    connectedClients.push(ws);
+
+    ws.on('message', data => {
+        connectedClients.forEach((ws, i) => {
+            if (ws.readyState === ws.OPEN) {
+                ws.send(data);
+            } else {
+                connectedClients.splice(i, 1);
+            }
+        })
+    });
+
+});
