783e8e823fdce2d42c0f365f6ebd874d6d76166c
cmarioep <cmescandonpardo@gmail.com>
2022-08-31 15:44:55 -0500

    Edit: setting to deploy on heroku

--- index.js
+++ index.js
@@ -6,36 +6,34 @@ const HTTP_PORT = process.env.PORT || 3000;
 const WS_PORT = 8888;
 
 
-httpServer.listen(HTTP_PORT, () => {
-    console.log(`HTTP server listening at ${HTTP_PORT}`);
-}
-);
-
-httpServer.get('/', (req, res) => {
-    res.send('Hello World! from WS Server');
-  });
-
 const wsServer = new WebSocket.Server({ port: WS_PORT }, () => {
     console.log(`WebSocket Server is listening at ${WS_PORT}`)
-}
-);
+});
 
 
 let connectedClients = [];
-
-wsServer.on('connection', (ws, req) => {
+wsServer.on('connection', (ws, req)=>{
     console.log('Connected');
     connectedClients.push(ws);
 
     ws.on('message', data => {
-        connectedClients.forEach((ws, i) => {
-            if (ws.readyState === ws.OPEN) {
+        connectedClients.forEach((ws,i)=>{
+            if(ws.readyState === ws.OPEN){
                 ws.send(data);
-            } else {
-                connectedClients.splice(i, 1);
+            }else{
+                connectedClients.splice(i ,1);
             }
         })
     });
+});
 
+
+httpServer.listen(HTTP_PORT, () => {
+    console.log(`HTTP server listening at ${HTTP_PORT}`);
+});
+
+
+httpServer.get('/', (req, res) => {
+    res.send('Hello World! from WS Server');
 });
 
